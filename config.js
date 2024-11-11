a9a125b66bc1dfad84408a30fddc83ec2fff1a9f
Huaa <hoafntxz01004@gmail.com>
2024-11-11 21:08:13 +0700

    Chi Yeu Minh Em

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Băng Đáng Yêuuuuuuuu",
+    introTitle: 'Bănggggggg ơiiii',
+    introDesc: `Băng xinh đẹp đáng yêuuuuuuu ơi <33333`,
+    btnIntro: 'Nhấn vô đây đi Bănggggg đáng iuuu^^',
+    title: 'Băng đánggg yêuuu xing gáiii ơi',
+    desc: 'Băng có yêu Hoà khônggggggggggggg??',
+    btnYes: 'Có, yêu rất nhiềuuu',
+    btnNo: 'Không nha :3',
+    question: 'Băng tặng quà noel cho Hoà nhaaaaaaa!<3',
+    btnReply: 'Gửi cho Hoàaaa <3',
+    reply: 'Co gi thi noi luon nhe',
+    mess: 'Hoà cũng yêu Băng rất nhiều <3',
+    messDesc: 'QLVHN',
+    btnAccept: 'Có đồng ý không ? <3.',
+    
+}
